6d153a3cc83fc6393974581ddeedfdb943c16eba
Jonathan Benn <bonjennprojects@gmail.com>
2025-06-25 12:43:42 -0700

    Initial Commit

---
+++ Vistaview/VistaviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VistaviewApp.swift
+//  Vistaview
+//
+//  Created by Jonathan Benn on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct VistaviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VistaviewTests/VistaviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  VistaviewTests.swift
+//  VistaviewTests
+//
+//  Created by Jonathan Benn on 6/25/25.
+//
+
+import Testing
+@testable import Vistaview
+
+struct VistaviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
